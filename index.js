f98a295c7945589e3b6f035411124f16ed64030f
Ayantu Etefa <118053020+Ayantu-Etefa@users.noreply.github.com>
2023-10-13 23:33:46 +0300

    Update index.js
    MongoDb Atlas connection has been created

--- index.js
+++ index.js
@@ -1,7 +1,86 @@
-const express = require('express');
-const app = express();
-const PORT = 8800;
+var express=require("express");
+var bodyParser=require("body-parser");
+const {MongoClient} = require('mongodb');
 
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost:27017/gfg');
+// var db=mongoose.connection;
+// db.on('error', console.log.bind(console, "connection error"));
+// db.once('open', function(callback){
+// 	console.log("connection succeeded");
+// })
+
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://admin:admin@cluster0.eqgvban.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+
+var app=express()
+
+
+app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+app.post('/sign_up', function(req,res){
+	var name = req.body.name;
+	var email =req.body.email;
+	var pass = req.body.password;
+	var phone =req.body.phone;
+
+	var data = {
+		"name": name,
+		"email":email,
+		"password":pass,
+		"phone":phone
+	}
+db.collection('details').insertOne(data,function(err, collection){
+		if (err) throw err;
+		console.log("Record inserted Successfully");
+			
+	});
+		
+	return res.redirect('signup_success.html');
+})
+
+
+app.get('/',function(req,res){
+res.set({
+	'Access-control-Allow-Origin': '*'
+	});
+return res.redirect('index.html');
+}).listen(3000)
+
+
+console.log("server listening at port 3000");
